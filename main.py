f47c672dd3a7946bae6aa1b8a2d730cf48d63920
MatthewMaat <matthewspamadres@gmail.com>
2023-07-17 13:33:14 +0200

    Turn into sink parity game

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from strategy_iteration import *
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    run_strategy_iteration_with_counterexample(3, counterexample='switchbest', imp_rule="othervaluation", mode="symmetric")
+    run_strategy_iteration_with_counterexample(3, counterexample='generalizedsymmetric', imp_rule="generalizedsymmetric", mode="symmetric")
 
 
 
